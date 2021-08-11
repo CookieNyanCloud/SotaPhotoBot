55d8b4ff510cc7a3f577f4ccf1b07f3f969e6152
CookieNyanCloud <emil8yunusov@gmail.com>
2021-08-11 20:33:53 +0300

    make it better

--- main.go
+++ main.go
@@ -18,8 +18,19 @@ const (
 	stiker      = "stiker.png"
 	stikercent  = "cetn.png"
 	stikerbok   = "chert.png"
+	newstick   = "newstick.png"
+
 	watermarked = "watermarked.jpeg"
 	webhook     = "https://photosotabot.herokuapp.com/"
+)
+
+const (
+	cb = "1"
+	cg = "2"
+	cw = "3"
+	sb = "4"
+	sg = "5"
+	sw = "6"
 )
 
 func main() {
@@ -43,19 +54,63 @@ func main() {
 
 	updates := bot.ListenForWebhook("/")
 
-	//bot.Debug = true
-	//log.Printf("Authorized on account %s", bot.Self.UserName)
-	//u := tgbotapi.NewUpdate(0)
-	//u.Timeout = 60
-	//updates, err := bot.GetUpdatesChan(u)
+
+	type usersState struct {
+		name string
+		command string
+	}
+	users:= make([]usersState, 0, 25)
+
 	for update := range updates {
-		if update.Message == nil { // ignore any non-Message Updates
+		//println(update.Message.IsCommand())
+		//println(users[0].id)
+		//if update.Message == nil { // ignore any non-Message Updates
+		//	continue
+		//}
+		//if (update.Message.Text) != "" {
+		//	continue
+		//}
+
+		exist := -1
+		userkol  := 0
+
+		if (update.Message.IsCommand()) {
+			println("comand")
+			for i := range users {
+				if users[i].name == update.Message.From.UserName {
+					exist = i
+					println("EXIST",exist)
+				}
+				userkol++
+			}
+			println(userkol)
+
+			if exist != -1 {
+				users[exist].command = update.Message.Command()
+			} else {
+				println("NOTEXIST",exist)
+				nowuser := usersState{
+					name:      update.Message.From.UserName,
+					command: update.Message.Command(),
+				}
+				users = append(users, nowuser)
+			}
+
 			continue
 		}
 
-		if (update.Message.Text) != "" {
+		if (update.Message.Text)!=""{
 			continue
 		}
+
+		for i := range users {
+			if users[i].name == update.Message.From.UserName {
+				exist = i
+			}
+		}
+
+		var varstick string
+		var offsetX, offsetY int
 
 		leng := len(*update.Message.Photo)
 		phUrl, err := bot.GetFileDirectURL((*update.Message.Photo)[leng-1].FileID)
@@ -64,24 +119,48 @@ func main() {
 		if err != nil {
 			err.Error()
 		}
-
 		imgb, _ := os.Open(filename)
 		img, _ := jpeg.Decode(imgb)
 		defer imgb.Close()
 
-		var varstick string
-		var offsetX, offsetY int
-		pos := update.Message.Caption
-		if pos == "1" {
+		println(users[exist].command)
+		com:= users[exist].command
+		switch com {
+		case cb:
 			varstick = stikerbok
-		} else if pos == "2" {
+		case cg:
 			varstick = stikercent
-		} else {
+		case cw:
+			varstick = stikercent
+		case sb:
 			varstick = stikerbok
+		case sg:
+			varstick = stikerbok
+		case sw:
+			varstick = stikerbok
+		default:
+			varstick = stikerbok
+
 		}
 
 		widthF, heightF := getImageDimension(filename)
+
+		//wff:=float64(widthF)
+		//hff:=float64(heightF)
+		//newWidthf:=math.Sqrt(((wff)*(hff))/4)
+		//newWidth:=int(math.Round(newWidthf))
+		//src, err := imaging.Open(varstick)
+		//src = imaging.Resize(src, newWidth, 0, imaging.Lanczos)
+		//err = imaging.Save(src, newstick)
+		//if err != nil {
+		//	log.Fatalf("failed to save image: %v", err)
+		//}
+
+		//widthS, heightS := getImageDimension(newstick)
 		widthS, heightS := getImageDimension(varstick)
+
+
+
 
 		switch varstick {
 		case stikerbok:
@@ -94,8 +173,8 @@ func main() {
 			offsetX = 0
 			offsetY = heightF - heightS
 		}
-
 		wmb, _ := os.Open(varstick)
+		//wmb, _ := os.Open(newstick)
 		watermark, _ := png.Decode(wmb)
 		defer wmb.Close()
 
@@ -106,7 +185,6 @@ func main() {
 		draw.Draw(m, watermark.Bounds().Add(offset), watermark, image.ZP, draw.Over)
 
 		imgw, _ := os.Create(watermarked)
-		//png.Encode(imgw,m)
 		err = jpeg.Encode(imgw, m, &jpeg.Options{jpeg.DefaultQuality})
 		if err != nil {
 			err.Error()
@@ -114,12 +192,14 @@ func main() {
 		}
 		defer imgw.Close()
 
-		//msg:= tgbotapi.NewPhotoUpload(update.Message.Chat.ID,"watermark-new-stiker.png")
 		msg := tgbotapi.NewPhotoUpload(update.Message.Chat.ID, "watermarked.jpeg")
 		msg.ReplyToMessageID = update.Message.MessageID
 		bot.Send(msg)
+		users[exist].command = ""
 
-		//prevname = filename
+
+
+
 	}
 }
 
